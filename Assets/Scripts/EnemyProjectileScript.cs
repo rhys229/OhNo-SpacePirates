1f4b8e6d4f9cc34937db79598b8664eeecf256db
rhys229 <60019827+rhys229@users.noreply.github.com>
2020-10-07 14:27:02 -0600

    10/7 Playtest take 2

---
+++ Assets/Scripts/EnemyProjectileScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileScript : MonoBehaviour
+{
+    public float               speed = 2f;
+    public int direction;
+    private Rigidbody2D        rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("Launch");
+    }
+
+    
+    private IEnumerator Launch() {
+        //yield return new WaitForSeconds(1);
+        //rb.AddForce(transform.right * -1);
+        rb.AddForce(transform.up * speed * direction);
+        yield return null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("We hit the player");
+        }
+
+        if (other.gameObject.tag == "wall")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "Ally")
+        {
+            Debug.Log("We Hit an Ally Ship");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ally")
+        {
+            //award points
+            Debug.Log("We Hit an Ally Ship");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TwinShooterScript.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TwinShooterScript : MonoBehaviour
+{
+    public GameObject twinProjectile;
+    public GameObject enemyTwinProjectile;
+    public int slot;
+    public bool playerControlled = false;
+    public GameObject playerObject;
+
+    public float speed = 1f;
+    public float yspeed = 2f;
+    public float amplitude = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.Find("Player");
+        float delay = Random.Range(2f, 10f);
+        float rate = Random.Range(2f, 8f);
+        InvokeRepeating("Fire", delay, rate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControlled == true)
+        {
+            
+            if (slot == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    Instantiate(twinProjectile, new Vector2(transform.position.x, transform.position.y+ 0.5f), Quaternion.identity);
+                }
+            }
+            else if (slot == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    Instantiate(twinProjectile, new Vector2(transform.position.x, transform.position.y+ 0.5f), Quaternion.identity);
+                }
+            }
+            else if (slot == 3)
+            {
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    Instantiate(twinProjectile, new Vector2(transform.position.x, transform.position.y+ 0.5f), Quaternion.identity);
+                }
+            }
+        }
+        else if (playerControlled == false)
+        {
+            float offsetx = Mathf.Sin(Time.time * speed) * amplitude / 20;
+            float offsety = -.2f*(Time.time * yspeed);
+            Vector3 move = new Vector3(offsetx, y: offsety,0);
+            transform.position = transform.position+move;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Abductor Collision");
+        if (other.gameObject.tag == "leftAbductor")
+        {
+            playerControlled = true;
+            transform.gameObject.tag = "Ally";
+            transform.position = new Vector2(playerObject.transform.position.x-.7f, playerObject.transform.position.y);
+            transform.Rotate(180,0,0);
+            transform.parent = playerObject.transform;
+            slot = 1;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "midAbductor")
+        {
+            playerControlled = true;
+            transform.gameObject.tag = "Ally";
+            transform.position = new Vector2(playerObject.transform.position.x, playerObject.transform.position.y);
+            transform.Rotate(180,0,0);
+            transform.parent = playerObject.transform;
+            slot = 2;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "rightAbductor")
+        {
+            playerControlled = true;
+            transform.gameObject.tag = "Ally";
+            transform.position = new Vector2(playerObject.transform.position.x+.7f, playerObject.transform.position.y);
+            transform.Rotate(180,0,0);
+            transform.parent = playerObject.transform;
+            slot = 3;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Fire()
+    {
+        int i = Random.Range(0, 100);
+        if (i > 80 && playerControlled == false) 
+        {
+            Instantiate(enemyTwinProjectile, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        }
+    }
+
+    public bool IsPlayerControlled()
+    {
+        return playerControlled;
+    }
+}
